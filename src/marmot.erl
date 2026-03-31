f63b577cdb0c8c33492f820a946e30d293553c23
chiroptical <chiroptical@gmail.com>
2026-03-31 10:08:59 -0400

    Add uuid package; continue with from_file

--- src/marmot.erl
+++ src/marmot.erl
@@ -2,11 +2,17 @@
 
 -export([from_file/1]).
 
--spec from_file(FileName :: string()) -> {ok, Contents :: binary()} | {error, Reason :: string()}.
+-record(untyped_query, {input_file_name :: string(), starting_line :: integer(), root_name :: file:filename_all(), file_content :: binary(), comments :: string()}).
+
+-spec from_file(FileName :: string()) -> {ok, UntypedQuery :: #untyped_query{}} | {error, Reason :: string()}.
 from_file(FileName) ->
     maybe
-        {ok, Contents} = file:read_file(FileName),
-        {ok, Contents}
+        {ok, Content} ?= file:read_file(FileName),
+        BaseName = filename:basename(FileName),
+        RootName = filename:rootname(BaseName),
+        % TODO: Check RootName is valid character set
+        % TODO: Pull comemnts from Content
+        {ok, #untyped_query{input_file_name = FileName, starting_line = 1, root_name = RootName, file_content = Content, comments = ""}}
     else
         {error, Reason} -> {error, Reason}
     end.
