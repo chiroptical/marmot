2af1f41067dc617fc30290aa3fdd238a277a782d
chiroptical <chiroptical@gmail.com>
2026-04-21 20:49:05 -0400

    Start working on infer_types

--- README.md
+++ README.md
@@ -3,12 +3,40 @@ marmot
 
 A pure Erlang implementation of [squirrel][squirrel]
 
-Plan
-----
+## Example
 
-- [ ] Implement `from_file`, L107 in src/squirrel/internal/query.gleam
-    Given a `Filename :: string`, return `{ok, UntypedQuery}` or `{error, ...}`
+Generated SQL may look like,
+
+```erlang
+-module(sql_generated).
+
+-export([get/1]).
+
+-spec get(SomeUuid :: uuid:uuid()) ->
+    {ok, OtherUuid :: uuid:uuid(), AnotherUuid :: uuid:uuid()}.
+get(SomeUuid) ->
+    %% ...
+    {ok, OtherUuid, AnotherUuid}.
+```
+
+Consumer,
+
+```erlang
+-module(consumer_module).
+
+-export([some_api/0]).
+
+-spec some_api() -> uuid:uuid().
+some_api() ->
+    {ok, OtherUuid, _AnotherUuid} = sql_generated:get(uuid:get_v4()),
+    OtherUuid.
+```
+
+## Plan
+
 - [ ] Implement `infer_types`, L544 src/squirrel/internal/database/postgres.gleam
     Given `UntypedQuery`, returns `{ok, TypedQuery}` or `{error, ...}` 
+    - [ ] Set up `pgo` architecture for making client requests
+    - [ ] Write and test `parameters_and_returns`
 
 [squirrel]: https://github.com/giacomocavalieri/squirrel
--- src/marmot.erl
+++ src/marmot.erl
@@ -1,10 +1,15 @@
 -module(marmot).
 
--export([from_file/1]).
+-export([
+    from_file/1,
+    infer_types/1,
+    parameters_and_returns/1
+]).
 
 -record(untyped_query, {
     input_file_name :: string(),
     starting_line :: integer(),
+    % TODO: root_name should be binary
     root_name :: file:filename_all(),
     file_content :: binary()
 }).
@@ -31,3 +36,71 @@ from_file(FileName) ->
     else
         {error, Reason} -> {error, Reason}
     end.
+
+-type type() ::
+    date
+    | {option, type()}
+    | date
+    | time_of_day
+    | timestamp
+    | bit_array
+    | int
+    | float
+    | numeric
+    | bool
+    | string
+    | json
+    | uuid:uuid()
+    | list(type()).
+
+-record(field, {
+    identifier :: string(),
+    type :: type()
+}).
+
+-record(typed_query, {
+    input_file_name :: string(),
+    starting_line :: integer(),
+    % TODO: root_name should be binary
+    root_name :: file:filename_all(),
+    content :: binary(),
+    params :: list(type()),
+    returns :: list(#field{})
+}).
+
+-spec infer_types(#untyped_query{}) ->
+    {ok, #typed_query{}}
+    | {error, Reason :: string()}.
+infer_types(UntypedQuery = #untyped_query{}) ->
+    % 1. Ask postgres for information about query parameters and returned rows
+    % 2. The parameters will allows us to turn OIDs into Erlang `type()`
+    % 3. Return types will give us OIDs too, but we can't know nullability
+    %    without reading the query plan
+    % Q: If we are unable to form a query plan, e.g. with a `do`, just assume
+    %    the returns are nullable?
+    {ok, #typed_query{
+        input_file_name = UntypedQuery#untyped_query.input_file_name,
+        starting_line = UntypedQuery#untyped_query.starting_line,
+        root_name = UntypedQuery#untyped_query.root_name,
+        content = UntypedQuery#untyped_query.file_content,
+        params = [],
+        returns = []
+    }}.
+
+-spec parameters_and_returns(#untyped_query{}) ->
+    {ok, nil}
+    | {error, Reason :: string()}.
+parameters_and_returns(_UntypedQuery = #untyped_query{}) ->
+    %% 1. Need a connection to make queries
+    %% 2. pgo_protocol:encode_parse_message/3
+    %% 3. pgo_protocol:encode_describe_message/2
+    %% 4. pgo_protocol:encode_sync_message/0
+    %% 5.
+    %% SocketModule can be ssl or gen_tcp, ideally we just have a pgo connection
+    %% case SocketModule:send(Socket, pgo_protocol:encode...(...)) of
+    %%     ok ->
+    %%         receive_message(SocketModule, Socket, Pool, []);
+    %%     {error, _} = SendError ->
+    %%         SendError
+    %% end.
+    {ok, nil}.
